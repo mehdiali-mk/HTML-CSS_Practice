dbb8f964711388c5a32eb0c3f4e6c5af7301d4d8
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-01-24 00:02:46 +0530

    Practice - Folder added

---
+++ Practice/script.js
@@ -0,0 +1,25 @@
+let maxWidth = window.screen.availWidth;
+let maxHeight = window.screen.availHeight;
+let newXPosition = [],
+  newYPosition = [];
+
+const dotsEl = document.querySelectorAll(".dot");
+
+function newPosition() {
+  for (let i = 0; i < 40; i++) {
+    newYPosition[i] = Math.floor(Math.random() * 100);
+    newXPosition[i] = Math.floor(Math.random() * 100);
+  }
+}
+console.log(dotsEl);
+function changePosition() {
+  newPosition();
+  for (let i = 0; i < 40; i++) {
+    dotsEl[i].style.top = newYPosition[i] + "%";
+    dotsEl[i].style.left = newXPosition[i] + "%";
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  changePosition(changePosition, 50);
+}
